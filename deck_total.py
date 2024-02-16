a038044390f8e952b5e47b698fe4a5653c9016c5
anastasia chudakova <chuan22git@gmail.com>
2024-02-16 21:11:12 +0300

    first commit

--- deck_total.py
+++ deck_total.py
@@ -1,91 +1,95 @@
 import random
-from typing import Self
+
 
 # Начнем с создания карты
 # ♥ ♦ ♣ ♠
-VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-SUITS = ('Spades', 'Clubs', 'Diamonds', 'Hearts')
-SUITS_UNI = {
-        'Spades': '♠',
-        'Clubs': '♣',
-        'Diamonds': '♦',
-        'Hearts': '♥'
-}
+VALUES = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+SUITS = ("Spades", "Clubs", "Diamonds", "Hearts")
+SUITS_UNI = {"Spades": "♠", "Clubs": "♣", "Diamonds": "♦", "Hearts": "♥"}
+
 
 class Card:
-    ''' Класс карта'''
+    """Класс карта"""
+
     def __init__(self, value: str, suit: str):
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
-        self.suit = suit    # Масть карты
+        self.suit = suit  # Масть карты
 
     def to_str(self):
-        '''Красивый вывод карты'''
-        return f'{self.value}{SUITS_UNI[self.suit]}'
-    
+        """Красивый вывод карты"""
+        return f"{self.value}{SUITS_UNI[self.suit]}"
+
     def __str__(self):
-        return f'{self.value}{SUITS_UNI[self.suit]}'
-    
+        return f"{self.value}{SUITS_UNI[self.suit]}"
+
     def __repr__(self):
-        return f'{self.value}{SUITS_UNI[self.suit]}'
+        return f"{self.value}{SUITS_UNI[self.suit]}"
 
     def equal_suit(self, other_card) -> bool:
-        '''Проверка являются ли масти карт одинаковые'''
-        return self.suit ==  other_card.suit 
-            
+        """Проверка являются ли масти карт одинаковые"""
+        return self.suit == other_card.suit
+
     def more(self, other_card) -> bool:
-        '''Проверка: является ли первая карта старше чем вторая.
-           Сначала сравниваются значения карты, потом масти'''
-        if self.value ==  other_card.value :
+        """Проверка: является ли первая карта старше чем вторая.
+        Сначала сравниваются значения карты, потом масти"""
+        if self.value == other_card.value:
             return SUITS.index(self.suit) > SUITS.index(other_card.suit)
-        return VALUES.index(self.value) > VALUES.index(other_card.value)              
+        return VALUES.index(self.value) > VALUES.index(other_card.value)
 
     def less(self, other_card) -> bool:
-        '''Проверка: является ли первая карта младше, чем вторая'''
-        if self.value ==  other_card.value :
+        """Проверка: является ли первая карта младше, чем вторая"""
+        if self.value == other_card.value:
             return SUITS.index(self.suit) < SUITS.index(other_card.suit)
         return VALUES.index(self.value) < VALUES.index(other_card.value)
-    
-    def __gt__(self, other:Self) -> bool:
-        return self.more(other)
-    
-    def __lt__(self, other:Self) -> bool:
-        return self.less(other)
-    
 
-#Теперь создадим колоду из 52-ух карт и реализуем все методы
+    def __gt__(self, other) -> bool:
+        return self.more(other)
+
+    def __lt__(self, other) -> bool:
+        return self.less(other)
+
+
+# Теперь создадим колоду из 52-ух карт и реализуем все методы
+
 
 class Deck:
-    '''Класс колода'''
+    """Класс колода"""
+
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
-        self.cards = [Card(value, suit)  for suit in SUITS for value in VALUES ]
-        
+        self.cards = [Card(value, suit) for suit in SUITS for value in VALUES]
+
     def show(self):
-        '''Отображает все карты колоды в формате:deck[12]: 3♥, 4♦, A♣, … 
-           где 12 - текущее кол-во карт в колоде.'''
-        return f'desk[{len(self.cards)}]: {", ".join([str(card) for card in self.cards])}'
-        
-    def draw(self, x:int) -> Self:
-        '''Возвращает x первых карт из колоды в виде списка, 
-           эти карты убираются из колоды. Уточнение: первую карту в списке считаем верхней картой колоды'''
+        """Отображает все карты колоды в формате:deck[12]: 3♥, 4♦, A♣, …
+        где 12 - текущее кол-во карт в колоде."""
+        return (
+            f'desk[{len(self.cards)}]: {", ".join([str(card) for card in self.cards])}'
+        )
+
+    def draw(self, x: int):
+        """Возвращает x первых карт из колоды в виде списка,
+        эти карты убираются из колоды. Уточнение: первую карту в списке считаем верхней картой колоды
+        """
         draw_cards = self.cards[:x]
         self.cards = self.cards[x:]
         return draw_cards
-            
+
     def shuffle(self):
         """Перетасовать колоду"""
         random.shuffle(self.cards)
-    
+
     def __str__(self):
-        return f'desk[{len(self.cards)}]: {", ".join([str(card) for card in self.cards])}'
-    
-    def __getitem__(self,index):
+        return (
+            f'desk[{len(self.cards)}]: {", ".join([str(card) for card in self.cards])}'
+        )
+
+    def __getitem__(self, index):
         return self.cards[index]
-    
+
     def __iter__(self):
         self.index = -1
-        return self 
-    
+        return self
+
     def __next__(self):
         self.index += 1
         if self.index < len(self.cards):
@@ -93,7 +97,7 @@ class Deck:
         else:
             raise StopIteration
 
-    
+
 # deck = Deck()
 # # Задачи - реализовать нативную работу с объектами:
 # # 1. Вывод колоды в терминал:
@@ -123,5 +127,3 @@ class Deck:
 # # 5. Просмотр карты в колоде по ее индексу:
 # # __getitem__(self, index):
 # print('Шестая карта:', deck[5])
-
-
